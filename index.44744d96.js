7afa0b4b341379586c374d8ac0ab73773cd45ae5
ZakZh <38009936+ZakZh@users.noreply.github.com>
2024-06-17 23:18:40 +0300

    make build

---
+++ index.44744d96.js
@@ -0,0 +1,2 @@
+document.querySelector(".wall>img").style.cssText="\n  top: 50%;\n  left: 50%;\n  transform: translate(-50%, -50%);\n";
+//# sourceMappingURL=index.44744d96.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,9 @@
+"use strict";
+const spiderImg = document.querySelector(".wall>img");
+spiderImg.style.cssText = `
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
+
+//# sourceMappingURL=index.f75de5e1.js.map
